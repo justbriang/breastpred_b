459a85692b22b089acae6cef41b7f804075bc147
justbrian <gichukxb@gmail.com>
2021-12-13 09:30:28 +0300

    static_dir

--- bcp_web/settings.py
+++ bcp_web/settings.py
@@ -125,9 +125,9 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
